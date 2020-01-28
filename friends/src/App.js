da54cbce8673332b379a56b40cd9a5f1e6b4e69b
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-28 15:03:27 -0500

    built header component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
+// styles
+import "./App.css";
+
+// components
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Switch>
+        <Route component={Login} />
+      </Switch>
     </div>
   );
 }
---
+++ friends/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+class Header extends React.Component {
+  render() {
+    <header>
+      <nav>
+        <NavLink to="/login">Login</NavLink>
+      </nav>
+    </header>;
+  }
+}
+
+export default Header;
